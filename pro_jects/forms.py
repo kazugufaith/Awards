ad615ab38bb614c4b5dbbfd8b1c67ffa5ce4e14f
kazugufaith <faithmugesia@gmail.com>
2019-07-03 11:09:23 +0300

    added a forms on my project

--- pro_jects/forms.py
+++ pro_jects/forms.py
@@ -1,12 +1,12 @@
-# from django import forms
-# from .models import Profile,Project
+from django import forms
+from .models import Profile,Project
 
-# class NewProfileForm(forms.ModelForm):
-#   class Meta:
-#     model=Profile
-#     exclude=['projects']
+class NewProfileForm(forms.ModelForm):
+  class Meta:
+    model=Profile
+    exclude=['projects']
 
-# class NewProjectForm(forms.ModelForm):
-#   class Meta:
-#     model=Project
-#     exclude=['user']
+class NewProjectForm(forms.ModelForm):
+  class Meta:
+    model=Project
+    exclude=['user']
