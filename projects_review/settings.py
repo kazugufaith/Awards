1f735af130e1da499fe242710db7bab2d8e5211c
kazugufaith <faithmugesia@gmail.com>
2019-07-03 10:52:12 +0300

    added rest_framework,bootsrap3 to installed apps in setting.py file

--- projects_review/settings.py
+++ projects_review/settings.py
@@ -62,18 +62,18 @@ ALLOWED_HOSTS = []
 
 # Application definition
 
-# INSTALLED_APPS = [
-#     'rest_framework.authtoken',
-#     'rest_framework',
-#     'bootstrap3',
-#     'pro_jects.apps.ProJectsConfig',
-#     'django.contrib.admin',
-#     'django.contrib.auth',
-#     'django.contrib.contenttypes',
-#     'django.contrib.sessions',
-#     'django.contrib.messages',
-#     'django.contrib.staticfiles',
-# ]
+INSTALLED_APPS = [
+    'rest_framework.authtoken',
+    'rest_framework',
+    'bootstrap3',
+    'pro_jects.apps.ProJectsConfig',
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+]
 
 MIDDLEWARE = [
     'whitenoise.middleware.WhiteNoiseMiddleware',
